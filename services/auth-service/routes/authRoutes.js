e36ca4abe39e440916d879f40065a44587dd804f
mehranbangash <221029@students.au.edu.pk>
2024-11-10 15:46:01 +0500

    user Services

---
+++ services/auth-service/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const router=express.Router();
+const authController=require('../controllers/authController');
+const authenticator=require('../middlewares/authMiddleware');
+
+
+router.post('/register',authController.registerUser);
+router.post('/login',authController.loginUser);
+router.get('/me',authenticator,authController.getUserDetails);
+router.patch('/me',authenticator,authController.updateUserDetails);
+router.post('/logout',authenticator,authController.logoutUser);
+router.get('/verify',authenticator,authController.getRole);
+
+
+module.exports=router;
